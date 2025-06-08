9e59987d5f864a0fd940b25bcf979593bf131e7a
khs <hs.kwan@okestro.com>
2025-06-08 20:42:10 +0900

    [feat]: trpc 설정 완료료

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+// "use client";
 
-export default function Home() {
+// import { trpc } from "@/trpc/client";
+import { HydrateClient, trpc } from "@/trpc/server";
+import PageClient from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  // const { data } = trpc.hello.useQuery({ text: "KHS" });
+  // const data = await trpc.hello({ text: "khs " });
+  void trpc.hello.prefetch({ text: "kHs" });
+
   return (
-    <div>
-      <Image src="/logo.svg" height={50} width={50} alt="Logo" />
-      <p className="text-xl font-semibold tracking-tight">FormTube</p>
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 }
